6b6099c0fa7d667dced1cafe20fade3e63297fde
gwh111 <173695508@qq.com>
2018-06-11 15:27:33 +0800

    Initial Commit

---
+++ testshadow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testshadow
+//
+//  Created by gwh on 2018/6/11.
+//  Copyright © 2018年 gwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testshadow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testshadow
+//
+//  Created by gwh on 2018/6/11.
+//  Copyright © 2018年 gwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
